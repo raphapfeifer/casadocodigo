0eae7b4c7113f83feb02956114c76d9bd4ca824f
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2018-03-28 00:06:01 -0300

    28/03/2018

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -14,7 +14,7 @@ public class AppWebConfiguration {
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver(){
 		InternalResourceViewResolver resolver =  new InternalResourceViewResolver();
-		resolver.setPrefix("WEB-INF/views/");
+		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		
 		return resolver;
